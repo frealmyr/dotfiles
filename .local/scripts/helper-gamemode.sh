ae924800f5bf67e31d0c8be353ff87c1df13b46c
Fredrick Myrvoll <fredrickmyrvoll@gmail.com>
2023-03-29 14:51:17 +0200

    moar tweaks

---
+++ .local/scripts/helper-gamemode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+case "$1" in
+  'start') swaymsg "output * adaptive_sync on" && swaymsg "output * mode 3840x1600@119.982Hz" ;;
+  'end')   swaymsg "output * adaptive_sync off" && swaymsg "output * mode 3840x1600@59.994Hz" ;;
+  *) echo 'Requires start or end as a parameter'
+esac
