683a66743cff33528037ba447f1f17ca94d2cbaa
Fredrick Myrvoll <fredrickmyrvoll@gmail.com>
2023-03-30 10:00:41 +0200

    set gamemode to 120Hz, default to 75Hz

--- .local/scripts/helper-gamemode.sh
+++ .local/scripts/helper-gamemode.sh
@@ -2,6 +2,6 @@
 
 case "$1" in
   'start') swaymsg "output * adaptive_sync on" && swaymsg "output * mode 3840x1600@119.982Hz" ;;
-  'end')   swaymsg "output * adaptive_sync off" && swaymsg "output * mode 3840x1600@59.994Hz" ;;
+  'end')   swaymsg "output * adaptive_sync off" && swaymsg "output * mode 3840x1600@74.977Hz" ;;
   *) echo 'Requires start or end as a parameter'
 esac
